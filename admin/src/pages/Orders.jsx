644d997482da4e13cc82de2d4eb93d4edcc5e061
Shakhawat Sami <shakhawat5ap22@gmail.com>
2026-06-23 19:56:59 +0600

    add order delete button

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -112,6 +112,8 @@ const Orders = ({ token }) => {
       </div>
 
       {/* ✅ confirmation modal */}
+
+      
       {confirmId && (
         <div className='fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center z-50'>
           <div className='bg-white rounded-xl p-6 w-80 shadow-xl text-center'>
