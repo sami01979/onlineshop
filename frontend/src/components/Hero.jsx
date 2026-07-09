a217872e94e4dc6553d00f937f7e8d59bcff22ed
Shakhawat Sami <shakhawat5ap22@gmail.com>
2026-07-09 13:28:46 +0600

    fix layout shift on hero banners with aspect-ratio

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -5,11 +5,21 @@ const Hero = () => {
     return (
         <div className='flex flex-col gap-0.5 sm:flex-row'>
              {/* Hero right side */}
-            <img className='w-full  sm:w-1/2 ' src={assets.first_banner} alt="" />
+            <img 
+              className='w-full sm:w-1/2 aspect-[1376/768]' 
+              src={assets.first_banner} 
+              alt="Anaibo grocery discount offers" 
+              width="1376" 
+              height="768" 
+            />
             {/* Hero left side */}
-            
-            <img className='w-full  sm:w-1/2 ' src={assets.second_banner} alt="" />
-           
+            <img 
+              className='w-full sm:w-1/2 aspect-[1376/768]' 
+              src={assets.second_banner} 
+              alt="Anaibo delivery service within 1-2 hours" 
+              width="1376" 
+              height="768" 
+            />
         </div>
     )
 }
