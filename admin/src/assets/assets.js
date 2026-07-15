f2da9f05d58f714ac5dd1d612f4d0d80c421a18b
Shakhawat Sami <shakhawat5ap22@gmail.com>
2026-07-15 12:49:14 +0600

    updated logo

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,7 +5,7 @@ import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.svg'
 import list_icn from './list_icn.png'
 import update_icn from './update_icn.png'
-import logo from './anaibo.png'
+import logo from './anaibo.jpg'
 export const assets = {
     logo,
     add_icon,
